d57963e832487cbf749937f7d10c42d321002b83
daressel <nevaliashkaoxxxy@gmail.com>
2024-05-05 23:41:36 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
+import { Datasource } from './datasource/datasource';
+import { DataSource } from './data-source/data-source';
 
 @Module({
   imports: [ItemModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Datasource, DataSource],
 })
 export class AppModule {}
